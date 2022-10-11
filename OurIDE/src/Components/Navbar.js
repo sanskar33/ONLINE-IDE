b543af9ed01f5ab15284145909414a8303cd3a53
sanskar33 <sanskarbhatt33@gmail.com>
2022-10-12 03:11:19 +0530

    first commit

---
+++ OurIDE/src/Components/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Select from 'react-select';
+import '../Components/Navbar.css';
+
+const Navbar = ({userLang, setUserLang, userTheme,
+				setUserTheme, fontSize, setFontSize}) => {
+	const languages = [
+		{ value: "c", label: "C" },
+		{ value: "cpp", label: "C++" },
+		{ value: "python", label: "Python" },
+		{ value: "java", label: "Java" },
+	];
+	const themes = [
+		{ value: "vs-dark", label: "Dark" },
+		{ value: "light", label: "Light" },
+	]
+	return (
+		<div className="navbar">
+			<h1>FastIDE</h1>
+
+			<Select options={languages} value={userLang}
+					onChange={(e) => setUserLang(e.value)}
+					placeholder={userLang} />
+					
+			<Select options={themes} value={userTheme}
+					onChange={(e) => setUserTheme(e.value)}
+					placeholder={userTheme} />
+			<label>Font Size</label>
+			<input type="range" min="18" max="30"
+				value={fontSize} step="2"
+				onChange={(e) => { setFontSize(e.target.value)}}/>
+                <div className="importit">
+			<button >
+				Import shared code 
+			</button>
+			</div>
+			<div className="shareit">
+			<button >
+				Share 
+			</button>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
